868c6dc71e8df3c769657ced680bc75c48c974d9
BrightonDube <BrightonDube@users.noreply.github.com>
2020-04-18 18:24:03 +0200

    use bcrypt

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 //jshint esversion: 8
 require("dotenv").config();
-const md5 = require("md5")
+//const md5 = require("md5") Don't use
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption"); don't use
 const path = require("path");
 const port = process.env.PORT;
 const uri = process.env.DB_CONN;
 const app = express();
 // const encKey = process.env.SOME_32BYTE_BASE64_STRING;
 // const sigKey = process.env.SOME_64BYTE_BASE64_STRING;
-console.log(md5("cjd8XMsNDw@94F7NnYT!WJwbpIxOpZ"))
+const bcrypt = require('bcrypt');//Recommended because of it's slowness.
+const saltRounds = 10; //too many rounds will slow our app down
 
-app.set("views", path.join(__dirname,"/views"));
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 //connect to mongoose
 mongoose.connect(uri, {
@@ -23,8 +24,8 @@ mongoose.connect(uri, {
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true
-}, (err)=>{
-    if(!err) console.log("Successfully connected to the database.");
+}, (err) => {
+    if (!err) console.log("Successfully connected to the database.");
     else console.log("Could not connect to the database");
 });
 //create user Schema
@@ -45,52 +46,65 @@ const userSchema = new mongoose.Schema({
 //     signingKey: sigKey,
 //     excludeFromEncryption: ['username']
 //   });
-  
+
 // Create user model
 const User = mongoose.model("User", userSchema);
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render("home", {});
 });
 
-app.get("/login", (req, res)=>{
+app.get("/login", (req, res) => {
     res.render("login", {});
 });
 
-app.get("/register", (req, res)=>{
+app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.post("/register", async (req, res)=>{
-    const user = new User({
-        username: req.body.username,
-        password: md5(req.body.password)
-    });
-    await user.save((err)=>{
-        if(err) {
-            console.log(err);
-            if(err.code === 11000) res.send("The user exists");
-        }else res.render("secrets");
-    });
-});
+app.post("/register", (req, res) => {
 
-app.post("/login", async(req, res)=>{
-    await User.findOne({
-        username: req.body.username       
-        }, (err, foundUser)=>{
-            if(!err){            
-            if(!foundUser) res.send("no user found with that email");
-            else if (foundUser.password === md5(req.body.password)) res.render("secrets");
-                 else res.send("You entered a wrong password");
-            }
-            else console.log(err);
+    bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+        // Store hash in your password DB.
+        const user = new User({
+            username: req.body.username,
+            password: hash
         });
+        await user.save((err) => {
+            if (err) {
+                console.log(err);
+                if (err.code === 11000) res.send("The user exists");
+            } else res.render("secrets");
+        });
+
+    });
+
 });
 
-app.get("/logout", (req, res)=>{
+app.post("/login", async (req, res) => {
+    await User.findOne({
+        username: req.body.username
+    }, (err, foundUser) => {
+        if (!err) {
+            if (!foundUser) res.send("invalid username or password");
+            else {
+                bcrypt.compare(req.body.password, foundUser.password, async (err, result) => {
+                    if (result) res.render("secrets");
+                    else res.send("Invalid username or password!");
+                });
+
+            }
+        }
+        else console.log(err);
+    });
+
+
+});
+
+app.get("/logout", (req, res) => {
     res.redirect("/");
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Listening for connections on port " + port);
 });
